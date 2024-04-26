69963cdd150fe6bef999bbdfd06267564d232369
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-26 10:46:58 +0100

    Update cliente_insert.php

--- cliente_insert.php
+++ cliente_insert.php
@@ -11,9 +11,9 @@
 
     <?php
 
-        try {
+        include 'conexao.php';
 
-            include 'conexao.php';
+        try {
 
             if ($_SERVER["REQUEST_METHOD"] === "GET") {
 
@@ -30,86 +30,102 @@
                 $contratoCli = $_GET['dd_contratoCliente'];
                 $obsCli = $_GET['area_observacoes'];
 
-                $sql = "SELECT ID FROM TAB_LOCALIDADE WHERE NOME = '$localCli'";
+                $sql = "SELECT N_CLIENTE FROM TAB_CLIENTE WHERE N_CLIENTE = '$numCli'";
                 $result = $conn->query($sql);
 
                 if ($result->num_rows > 0) {
 
-                    while($row = $result->fetch_assoc()) {
-
-                        $localCli = $row['ID'];
-
-                    }
+                    echo "<div>
+                                <h2>Cliente $numCli já existe em nosso sistema</h2>
+                                <button type='button' onclick='redirect()'>Ok</button>
+                        </div>";
 
                 } else {
 
-                    throw new Exception("[Erro 401] ao Buscar Informações de Localidade");
+                    $sql = "SELECT ID FROM TAB_LOCALIDADE WHERE NOME = '$localCli'";
+                    $result = $conn->query($sql);
 
-                }
+                    if ($result->num_rows > 0) {
 
-                if ($contratoCli == "Sim") {
+                        while($row = $result->fetch_assoc()) {
 
-                    $tipoContratoCli = $_GET['dd_tipoContratoCliente'];
-                    $tempoTotalCli = $_GET['txt_tempoTotalCliente'];
-                    $tempoExtraCli = $_GET['txt_tempoExtraCliente'];
-                    $deslocacaoCli = $_GET['dd_deslocacaoCliente'];
+                            $localCli = $row['ID'];
 
-                    $stmt = $conn->prepare("INSERT INTO TAB_CONTRATO (ID, TIPO_CONTRATO, TEMPO_TOTAL, TEMPO_EXTRA, DESLOCACAO)
-                    VALUES (?, ?, ?, ?, ?)");
-                    $stmt->bind_param('isiis', $nifCli, $tipoContratoCli, $tempoTotalCli, $tempoExtraCli, $deslocacaoCli);
-                    $resultContrato = $stmt->execute(); 
-
-                    $stmt = $conn->prepare("INSERT INTO TAB_CLIENTE (N_CLIENTE, NIF, NOME, MORADA, CODIGO_POSTAL, ID_LOCALIDADE, CONTACTO_F, CONTACTO_M, CRIADO_DATA, ID_CONTRATO, EMAIL, STATUS, OBSERVACOES)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, ?, ?, ?)");
-                    $stmt->bind_param('iisssississs', $numCli, $nifCli, $nomeCli, $moradaCli, $codPostalCli, $localCli, $telFoneCli, $telMovelCli, $nifCli, $emailCli, $statusCli, $obsCli);
-                    $resultCadastro = $stmt->execute(); 
-
-                    if ($resultCadastro && $resultContrato) {
-
-                        echo "<div>
-                                <h2>Cliente $numCli Cadastrado</h2>
-                                <button type='button' onclick='redirect()'>Ok</button>
-                            </div>";
+                        }
 
                     } else {
+
+                        throw new Exception("[Erro 401] ao Buscar Informações de Localidade");
+
+                    }
+
+                    if ($contratoCli == "Sim") {
+
+                        $tipoContratoCli = $_GET['dd_tipoContratoCliente'];
+                        $tempoTotalCli = $_GET['txt_tempoTotalCliente'];
+                        $tempoExtraCli = $_GET['txt_tempoExtraCliente'];
+                        $deslocacaoCli = $_GET['dd_deslocacaoCliente'];
+
+                        $stmt = $conn->prepare("INSERT INTO TAB_CONTRATO (ID, TIPO_CONTRATO, TEMPO_TOTAL, TEMPO_EXTRA, DESLOCACAO)
+                        VALUES (?, ?, ?, ?, ?)");
+                        $stmt->bind_param('isiis', $nifCli, $tipoContratoCli, $tempoTotalCli, $tempoExtraCli, $deslocacaoCli);
+                        $resultContrato = $stmt->execute(); 
+
+                        $stmt = $conn->prepare("INSERT INTO TAB_CLIENTE (N_CLIENTE, NIF, NOME, MORADA, CODIGO_POSTAL, ID_LOCALIDADE, CONTACTO_F, CONTACTO_M, CRIADO_DATA, ID_CONTRATO, EMAIL, STATUS, OBSERVACOES)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, ?, ?, ?)");
+                        $stmt->bind_param('iisssississs', $numCli, $nifCli, $nomeCli, $moradaCli, $codPostalCli, $localCli, $telFoneCli, $telMovelCli, $nifCli, $emailCli, $statusCli, $obsCli);
+                        $resultCadastro = $stmt->execute(); 
+
+                        if ($resultCadastro && $resultContrato) {
+
+                            echo "<div>
+                                    <h2>Cliente $numCli Cadastrado</h2>
+                                    <button type='button' onclick='redirect()'>Ok</button>
+                                </div>";
+
+                        } else {
+                        
+                            throw new Exception("[Erro 402] ao Cadastrar Cliente");
+
+                        }
+
+                    } else {
+
+                        $stmt = $conn->prepare("INSERT INTO TAB_CLIENTE (N_CLIENTE, NIF, NOME, MORADA, CODIGO_POSTAL, ID_LOCALIDADE, CONTACTO_F, CONTACTO_M, CRIADO_DATA, EMAIL, STATUS, OBSERVACOES)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, ?, ?)");
+                        $stmt->bind_param('iisssisssss', $numCli, $nifCli, $nomeCli, $moradaCli, $codPostalCli, $localCli, $telFoneCli, $telMovelCli, $emailCli, $statusCli, $obsCli);
+                        $resultCadastro = $stmt->execute(); 
+
+                        if ($resultCadastro) {
+                            
+                            echo "<div>
+                                    <h2>Cliente $numCli Cadastrado</h2>
+                                    <button type='button' onclick='redirect()'>Ok</button>
+                                </div>";
+
+                        } else {
+                            
+                            throw new Exception("[Erro 402] ao Cadastrar Cliente");
+
+                        }
+
+                   }
+
+                }
                     
-                        throw new Exception("[Erro 402] ao Cadastrar Cliente");
-
-                    }
-
                 } else {
 
-                    $stmt = $conn->prepare("INSERT INTO TAB_CLIENTE (N_CLIENTE, NIF, NOME, MORADA, CODIGO_POSTAL, ID_LOCALIDADE, CONTACTO_F, CONTACTO_M, CRIADO_DATA, EMAIL, STATUS, OBSERVACOES)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, ?, ?)");
-                    $stmt->bind_param('iisssisssss', $numCli, $nifCli, $nomeCli, $moradaCli, $codPostalCli, $localCli, $telFoneCli, $telMovelCli, $emailCli, $statusCli, $obsCli);
-                    $resultCadastro = $stmt->execute(); 
-
-                    if ($resultCadastro) {
-                        
-                        echo "<div>
-                                <h2>Cliente $numCli Cadastrado</h2>
-                                <button type='button' onclick='redirect()'>Ok</button>
-                            </div>";
-
-                    } else {
-                        
-                        throw new Exception("[Erro 402] ao Cadastrar Cliente");
-
-                    }
+                    throw new Exception("[Erro 400] na Transmissão de Informações Web ");
 
                 }
-                
-            } else {
-
-                throw new Exception("[Erro 400] na Transmissão de Informações Web ");
-
-            }
 
         } catch (Exception $e) {
 
             throw new Exception("[Erro 400] na Transmissão de Informações Web ");
 
         }
+
+        $conn->close();
        
     ?>
 
