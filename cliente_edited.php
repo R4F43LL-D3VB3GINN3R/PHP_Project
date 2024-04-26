c6cb1644e13c63d5c6cf3a6598220693244c7c21
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-26 16:46:36 +0100

    Update cliente_edited.php

--- cliente_edited.php
+++ cliente_edited.php
@@ -30,6 +30,8 @@
                 $contratoCli = $_GET['dd_contratoCliente'];
                 $obsCli = $_GET['area_observacoes'];
 
+                //Consulta a linha referente ao número do cliente...
+
                 $sql = "SELECT * FROM TAB_CLIENTE WHERE N_CLIENTE = '$numCli'";
                 $result = $conn->query($sql);
 
@@ -40,21 +42,22 @@
                                 <button type='button' onclick='redirect()'>Ok</button>
                         </div>";
 
-                } else {
+                } else { //Se tudo correr bem com a consulta...
 
-                    $dadosCli = $result->fetch_assoc();
+                    $dadosCli = $result->fetch_assoc(); //Insere o cliente no vetor...
 
-                    $sql = "SELECT ID FROM TAB_LOCALIDADE WHERE NOME = '$localCli'";
+                    $sql = "SELECT ID FROM TAB_LOCALIDADE WHERE NOME = '$localCli'"; //Consulta o id da localidade do cliente...
                     $result = $conn->query($sql);
 
                     if ($result->num_rows > 0) {
 
                         while($row = $result->fetch_assoc()) {
 
-                            $localCli = $row['ID'];
+                            $localCli = $row['ID']; //Converte a variável no id...
 
                         }
 
+                        //Se o cliente tiver um contrato e quiser alterá-lo...
                         if ($dadosCli['ID_CONTRATO'] == $nifCli && $contratoCli == "Sim") {
 
                             $tipoContratoCli = $_GET['dd_tipoContratoCliente'];
@@ -82,12 +85,14 @@
     
                             } else {
                             
-                                throw new Exception("[Erro 402] ao modificar dados do Cliente");
+                                throw new Exception("[Erro] Ao modificar dados do Cliente");
     
                             }
 
+                        //Se o cliente tiver um contrato e não o quiser mais...
                         } else if ($dadosCli['ID_CONTRATO'] == $nifCli && $contratoCli == "Não") {            
 
+                            //Removemos as duas tabelas, mas a do cliente tem de ir primeiro...
                             $sql = "DELETE FROM TAB_CLIENTE WHERE NIF = '$nifCli'";
                             $resultDelete = $conn->query($sql); 
 
@@ -96,6 +101,7 @@
 
                             if ($resultDelete) {
 
+                                //Recadastramos o cliente no sistema...
                                 $stmt = $conn->prepare("INSERT INTO TAB_CLIENTE (N_CLIENTE, NIF, NOME, MORADA, CODIGO_POSTAL, ID_LOCALIDADE, CONTACTO_F, CONTACTO_M, CRIADO_DATA, EMAIL, STATUS, OBSERVACOES)
                                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
                                 $stmt->bind_param('iisssissssss', $numCli, $nifCli, $nomeCli, $moradaCli, $codPostalCli, $localCli, $telFoneCli, $telMovelCli, $dadosCli['ATUALIZACAO'], $emailCli, $statusCli, $obsCli);
@@ -104,14 +110,23 @@
                                 if ($resultCadastro) {
 
                                     echo "<div>
-                                            <h2>Dados do $numCli Alterados e Contrato Removido</h2>
+                                            <h2>Dados do Cliente $numCli Alterados e Contrato Removido</h2>
                                             <button type='button' onclick='redirect()'>Ok</button>
                                         </div>";
 
-                                } 
+                                } else {
+
+                                    throw new Exception("[Erro] Ao modificar dados do Cliente");
+
+                                }
+
+                            } else {
+
+                                throw new Exception("[Erro] Ao Remover dados do Cliente e Contrato");
 
                             }
-
+                        
+                        //Se o cliente não tiver um contrato, mas agora quer ter...
                         } else if ($dadosCli['ID_CONTRATO'] == NULL && $contratoCli == "Sim") {
 
                             $tipoContratoCli = $_GET['dd_tipoContratoCliente'];
@@ -119,11 +134,13 @@
                             $tempoExtraCli = $_GET['txt_tempoExtraCliente'];
                             $deslocacaoCli = $_GET['dd_deslocacaoCliente'];
 
+                            //Inserimos seu contrato primeiro...
                             $stmt = $conn->prepare("INSERT INTO TAB_CONTRATO (ID, TIPO_CONTRATO, TEMPO_TOTAL, TEMPO_EXTRA, DESLOCACAO)
                             VALUES (?, ?, ?, ?, ?)");
                             $stmt->bind_param('isiis', $nifCli, $tipoContratoCli, $tempoTotalCli, $tempoExtraCli, $deslocacaoCli);
                             $resultContrato = $stmt->execute(); 
 
+                            //Atualizamos o cliente para receber o contrato...
                             $stmt = $conn->prepare("UPDATE TAB_CLIENTE
                                                     SET N_CLIENTE = ?, NIF = ?, NOME = ?, MORADA = ?, CODIGO_POSTAL = ?, ID_LOCALIDADE = ?, CONTACTO_F = ?, CONTACTO_M = ?, ID_CONTRATO = ?, EMAIL = ?, STATUS = ?, OBSERVACOES = ? WHERE N_CLIENTE = ?");
                             $stmt->bind_param('iisssississsi', $numCli, $nifCli, $nomeCli, $moradaCli, $codPostalCli, $localCli, $telFoneCli, $telMovelCli, $nifCli, $emailCli, $statusCli, $obsCli, $numCli);
@@ -132,18 +149,20 @@
                             if ($resultCadastro && $resultContrato) {
     
                                 echo "<div>
-                                        <h2>Dados do $numCli Alterados e Contrato Criado</h2>
+                                        <h2>Dados do Cliente $numCli Alterados e Contrato Criado</h2>
                                         <button type='button' onclick='redirect()'>Ok</button>
                                     </div>";
     
                             } else {
                             
-                                throw new Exception("[Erro 402] ao modificar dados do Cliente");
+                                throw new Exception("[Erro] ao modificar Dados do Cliente");
     
                             }
-
+                        
+                        //Se o cliente não tinha um contrato e quer apenas atualizar as suas informações...
                         } else if ($dadosCli['ID_CONTRATO'] == NULL && $contratoCli == "Não") { 
 
+                            //Atualiza o cliente deixando de fora seu ID...
                             $stmt = $conn->prepare("UPDATE TAB_CLIENTE
                                                     SET N_CLIENTE = ?, NIF = ?, NOME = ?, MORADA = ?, CODIGO_POSTAL = ?, ID_LOCALIDADE = ?, CONTACTO_F = ?, CONTACTO_M = ?, EMAIL = ?, STATUS = ?, OBSERVACOES = ? WHERE N_CLIENTE = ?");
                             $stmt->bind_param('iisssisssssi', $numCli, $nifCli, $nomeCli, $moradaCli, $codPostalCli, $localCli, $telFoneCli, $telMovelCli, $emailCli, $statusCli, $obsCli, $numCli);
@@ -162,13 +181,25 @@
     
                             }
 
+                        } else {
+
+                            throw new Exception("[Erro] Ao Atualizar Dados de Clientes");
+
                         }
+
+                    } else {
+
+                        throw new Exception("[Erro] Ao acessar as Localidades");
 
                     }
 
                 }
 
-            }       
+            } else {
+
+                throw new Exception("[Erro] No envio de formulário Web.");
+
+            }     
 
         } catch (Exception $e) {
 
