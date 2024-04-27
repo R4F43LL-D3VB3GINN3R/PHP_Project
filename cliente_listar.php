a78c84c5b2b044eaac30dda3df4d6908c0bdace7
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-28 00:14:18 +0100

    Update cliente_listar.php

--- cliente_listar.php
+++ cliente_listar.php
@@ -1,151 +1,63 @@
-.layout {
-    width: 90%;
-    height: 600px;
-  
-    display: grid;
-    grid:
-      "name1 name1 name1" 1fr
-      "name2 name3 name3" 1fr
-      "name2 name3 name3" 1fr
-      / 1fr 1fr 1fr;
-    gap: 0px;
-  }
-  
-  .name1 { grid-area: name1; }
-  .name2 { grid-area: name2; }
-  .name3 { grid-area: name3; }
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../imgs/ipicon.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="../style/cliente_listar.css">
+    <title>Clientes</title>
+</head>
+<body>
+    <section class="layout">
 
-body {
-    height: 700px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    background-color: #dfdfee;
-    background-image: 
-        radial-gradient(at 47% 33%, hsl(180.00, 90%, 51%) 0, transparent 59%), 
-        radial-gradient(at 82% 65%, hsl(190.46, 52%, 59%) 0, transparent 55%);
-}
+    <?php //Menu Lateral...?>
 
-.name2 {
-    height: 600px;
-    width: 210px;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    justify-content: center;
-    font-size: 15px;
-    font-family: 'Roboto Mono', monospace;
-    font-weight: bold;
-}
+    <div class="name2">
+        <form action="cliente.php" id="form_name2" method="get">
+            <h2>Procurar</h2>
+            <input type="text" name="procuraCli" id="procuraCli">
+            <input type="submit" value="Procurar Nº" id="submit">
+            <button type="button" onclick='redirect_listar()'>Listar</button>
+            <h2>Menu</h2>
+            <button id="bt_dashboard">Dashboard</button>
+            <button type="button" onclick='redirect_cliente()' id="bt_cliente">Cliente</button>
+            <button type="button" onclick='redirect_fo()' id="bt_fo">Folhas de Obras</button>
+            <h2>Equipamento</h2>
+            <button id="bt_tipo">Tipo</button>
+            <button id="bt_marca">Marca</button>
+            <button id="bt_modelo">Modelo</button>
+            <button id="bt_catalogo">Catálogo</button>
+        </form>
+    </div>
 
-.name3 {
-    width: 1150px;
-    height: 600px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    font-size: 15px;
-    font-family: 'Roboto Mono', monospace;
-    font-weight: bold;
-}
+    <div class="name3">
+    </div>   
 
-#form_name2 {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    padding: 5px;
-}
+    </section>
 
-input {
-    width: 150px;
-    font-size: 15px;
-    font-family: 'Roboto Mono', monospace;
-    font-weight: bold;
-    cursor: pointer;
-}
+<script>
 
-select {
-    width: 150px;
-    font-size: 15px;
-    font-family: 'Roboto Mono', monospace;
-    font-weight: bold;
-    cursor: pointer;
-}
+    //Funções de redirecionamento...
 
-#submit, #listar_fo {
-    height: 35px;
-    width: 160px;
-    padding: 5px;
-    border-radius: 10px;
-    text-align: center;
-}
+    function redirect_cliente() {
 
-#bt_tipo, #bt_marca, #bt_modelo, #bt_catalogo {
-    margin-top: 5px;
-}
+        window.location.href = 'cliente.php';
 
-button {
-    width: 160px;
-    height: 35px;
-    font-size: 15px;
-    font-family: 'Roboto Mono', monospace;
-    font-weight: bold;
-    cursor: pointer;
-}
+    }
 
-.layout {
-    backdrop-filter: blur(16px) saturate(180%);
-    -webkit-backdrop-filter: blur(16px) saturate(180%);
-    background-color: rgb(199, 255, 250);
-    border-radius: 12px;
-}
+    function redirect_fo() {
 
-.name1, .name2, .name3 {
-    backdrop-filter: blur(16px) saturate(180%);
-    -webkit-backdrop-filter: blur(16px) saturate(180%);
-    background-color: rgb(144, 238, 254);
-    border-radius: 12px;
-    border: 2px solid rgb(255, 255, 255);
-}
+        window.location.href = 'fo.php';
 
-#form_name2 {
-    backdrop-filter: blur(16px) saturate(180%);
-    -webkit-backdrop-filter: blur(16px) saturate(180%);
-    background-color: rgb(83, 218, 255);
-    border-radius: 12px;
-    border: 2px solid rgb(255, 255, 255);
-    width: 180px;
-    height: 580px;
-}
+    }
 
-input , select, textarea{
-    background-color: rgb(255, 255, 255);
-    color: black;
-    border: 2px solid rgb(0, 75, 226);
-}
+    function redirect_listar() {
 
-label{
-    color: rgb(255, 255, 255);
-}
+        window.location.href = 'cliente_listar.php';
 
-h2 {
-    color: rgb(255, 255, 255);
-}
+    }
 
-button , #submit, #bt_cliente, #bt_fo{
-    backdrop-filter: blur(16px) saturate(180%);
-    -webkit-backdrop-filter: blur(16px) saturate(180%);
-    background-color: rgb(0, 170, 255);
-    border-radius: 12px;
-    border: 3px solid rgb(255, 255, 255);
-    color:rgb(255, 255, 255);
-    margin-top: 5px;
-}
+</script>
 
-button:hover , #submit:hover, #bt_cliente:hover, #bt_fo:hover{
-    background-color: rgb(255, 255, 255);
-    border: 3px solid rgb(0, 170, 255);
-    color:rgb(0, 170, 255);
-}
+</body>
+</html>
