32ed472ae5a7efe5f2292c06a6c50630d3380da6
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-28 02:32:54 +0100

    Update cliente.php

--- cliente.php
+++ cliente.php
@@ -17,15 +17,13 @@
         <h2>Procurar</h2>
         <input type="text" name="procuraCli" id="procuraCli">
         <input type="submit" value="Procurar Nº" id="submit">
-        <button id="listar_fo">Listar</button>
+        <button type="button" onclick='redirect_listar()'>Listar</button>
         <h2>Menu</h2>
         <button id="bt_dashboard">Dashboard</button>
         <button type="button" onclick='redirect_cliente()' id="bt_cliente">Cliente</button>
         <button type="button" onclick='redirect_fo()' id="bt_fo">Folhas de Obras</button>
         <h2>Equipamento</h2>
-        <button id="bt_tipo">Tipo</button>
-        <button id="bt_marca">Marca</button>
-        <button id="bt_modelo">Modelo</button>
+        <button id="bt_gerenciar">Gerenciar</button>
         <button id="bt_catalogo">Catálogo</button>
     </form>
 </div>
@@ -131,6 +129,7 @@
             <select name="dd_contratoCliente" id="dd_contratoCliente" onchange="showContratoFields()">
                 <option value="Não">Não</option>
                 <option value="Sim">Sim</option>
+
             </select>
         </form>
     </div>
@@ -426,6 +425,12 @@
     function redirect_fo() {
 
         window.location.href = 'fo.php';
+
+    }
+
+    function redirect_listar() {
+
+        window.location.href = 'cliente_listar.php';
 
     }
 
