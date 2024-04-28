b4df715ed03b4212e98a827dbc7ccd725e7b69fe
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-28 21:20:19 +0100

    Update fo.php

--- fo.php
+++ fo.php
@@ -21,7 +21,7 @@
         <button type="button" onclick='redirect_cliente()' id="bt_cliente">Cliente</button>
         <button type="button" onclick='redirect_fo()' id="bt_fo">Folhas de Obras</button>
         <h2>Equipamento</h2>
-        <button id="bt_gerenciar">Gerenciar</button>
+        <button type="button" onclick='redirect_equipamento()' id="bt_gerenciar">Gerenciar</button>
         <button id="bt_catalogo">Catálogo</button>
     </form>
 </div>
@@ -30,11 +30,59 @@
     <div class="subdiv1">
         <form action="fo2.php" method="post" id="form_subdiv1">
             <label for="txt_cliente">Cliente: </label>
-            <input type="text" name="txt_cliente">
+            
+            <?php 
+                
+                    echo '<select name="dd_cliente" id="cliente">';
+
+                    include 'conexao.php';
+
+                    $sql = "SELECT NOME FROM TAB_CLIENTE";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+
+                        while ($row = $result->fetch_assoc()) {
+
+                            echo '<option value=' . $row['NOME'] . '>' . $row['NOME'] . '</option>';
+
+                        }
+
+                    }
+                    
+                    echo '</select>';
+
+                    $conn->close();
+
+            ?>
+
             <label for="dd_tecnico">Atribuir: </label>
-            <select name="dd_tecnico" id="dd_tecnico">
-                <option value="tecnico">Técnico</option>
-            </select>
+            
+            <?php 
+                
+                    echo '<select name="dd_tecnico" id="tecnico">';
+
+                    include 'conexao.php';
+
+                    $sql = "SELECT NICK FROM TAB_TECNICO";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+
+                        while ($row = $result->fetch_assoc()) {
+
+                            echo '<option value=' . $row['NICK'] . '>' . $row['NICK'] . '</option>';
+
+                        }
+
+                    }
+                    
+                    echo '</select>';
+
+                    $conn->close();
+
+            ?>
+
             <label for="dd_orcamento">Orçamento:</label>
             <select name="dd_orcamento" id="dd_orcamento">
                 <option value="orcamento">Orçamento</option>
@@ -45,17 +93,86 @@
     <div class="subdiv2">
         <form action="fo2.php" method="post" id="form_subdiv2">
             <label for="dd_equipamento">Tipo Equip:</label>
-            <select name="dd_equipamento" id="dd_equipamento">
-                <option value="equipamento">Equipamento</option>
-            </select>
+            
+            <?php 
+                
+                    echo '<select name="dd_equipamento" id="equipamento">';
+
+                    include 'conexao.php';
+
+                    $sql = "SELECT NOME FROM TAB_TIPO";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+
+                        while ($row = $result->fetch_assoc()) {
+
+                            echo '<option value=' . $row['NOME'] . '>' . $row['NOME'] . '</option>';
+
+                        }
+
+                    }
+                    
+                    echo '</select>';
+
+                    $conn->close();
+
+                ?>
+
             <label for="dd_marca">Marca Equip:</label>
-            <select name="dd_marca" id="dd_marca">
-                <option value="marca">Marca</option>
-            </select>
+            
+            <?php 
+                
+                    echo '<select name="dd_marca" id="marca">';
+
+                    include 'conexao.php';
+
+                    $sql = "SELECT NOME FROM TAB_MARCA";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+
+                        while ($row = $result->fetch_assoc()) {
+
+                            echo '<option value=' . $row['NOME'] . '>' . $row['NOME'] . '</option>';
+
+                        }
+
+                    }
+                    
+                    echo '</select>';
+
+                    $conn->close();
+
+                ?>
+
             <label for="dd_modelo">Mod Equip:</label>
-            <select name="dd_modelo" id="dd_modelo">
-                <option value="modelo">Modelo</option>
-            </select>
+            
+            <?php 
+                
+                    echo '<select name="dd_modelo" id="modelo">';
+
+                    include 'conexao.php';
+
+                    $sql = "SELECT NOME FROM TAB_MODELO";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+
+                        while ($row = $result->fetch_assoc()) {
+
+                            echo '<option value=' . $row['NOME'] . '>' . $row['NOME'] . '</option>';
+
+                        }
+
+                    }
+                    
+                    echo '</select>';
+
+                    $conn->close();
+
+                ?>
+
         </form>
     </div>
           
@@ -84,8 +201,8 @@
             <label for="txt_requerimento">Nº Requerimento:</label>
             <input type="text" name="txt_requerimento">
             <label for="txt_numero_serie">Nº Série:</label>
-            <input type="text" name="txt_numero_serie">
-            <button id="bt_gen">Gerar</button>
+            <input type="text" name="txt_numero_serie" id="txt_numero_serie">
+            <button type="button" id="bt_gen" onclick='generatenum()'>Gerar</button>
         </form>
     </div> 
 
@@ -97,26 +214,64 @@
 </section>
 
 <script>
-    // Adiciona um evento de clique para o botão "Enviar Todos"
+
     document.getElementById("enviarTodos").addEventListener("click", function() {
-        var forms = document.querySelectorAll("form"); // Seleciona todos os formulários na página
-        var formData = new FormData(); // Cria um objeto FormData para armazenar os dados
 
-        // Loop através de todos os formulários
+        var forms = document.querySelectorAll("form"); 
+        var formData = new FormData(); 
+
         forms.forEach(function(form) {
-            var inputs = form.querySelectorAll("input, select, textarea"); // Seleciona todos os campos de entrada no formulário
+            var inputs = form.querySelectorAll("input, select, textarea");
 
-            // Loop através de todos os campos de entrada no formulário atual
             inputs.forEach(function(input) {
-                formData.append(input.name, input.value); // Adiciona o nome e o valor de cada campo de entrada ao objeto FormData
+
+                formData.append(input.name, input.value); 
+
             });
+            
         });
 
-        // Redireciona para a página fo2.php com os dados passados via URL
         window.location.href = "fo2.php?" + new URLSearchParams(formData).toString();
     });
+
 </script>
+
 <script>
+
+function generateRandomNumber(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+function generateSerialNumber(length) {
+
+    var charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var serialNumber = '';
+
+    for (var i = 0; i < length; i++) {
+
+        var randomIndex = generateRandomNumber(0, charset.length - 1);
+        serialNumber += charset[randomIndex];
+
+    }
+
+    return serialNumber;
+
+}
+
+function generatenum() {
+
+    var serialNumber = generateSerialNumber(10); 
+
+    document.getElementById('txt_numero_serie').value = serialNumber;
+
+}
+
+</script>
+
+<script>
+
     function redirect_cliente() {
 
         window.location.href = 'cliente.php';
@@ -134,6 +289,13 @@
          window.location.href = 'cliente_listar.php';
 
     }
+
+    function redirect_equipamento() {
+
+        window.location.href = 'equipamentos.php';
+
+    }
+
 </script>
 
 </body>
