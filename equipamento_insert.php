35563efdd51e90e4974b5b66df4476061f09a282
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-28 19:29:26 +0100

    Update equipamento_insert.php

--- equipamento_insert.php
+++ equipamento_insert.php
@@ -59,7 +59,96 @@
 
                     throw new Exception("Não foi possível inserir os dados ");
 
+                } 
+
+            } else if (isset($_POST['add_marca'])) {
+
+                $marca = $_POST['txt_marca'];
+
+                if($marca != NULL) {
+
+                    $stmt = $conn->prepare("INSERT INTO TAB_MARCA (NOME)
+                    VALUES (?)");
+                    $stmt->bind_param('s', $marca);
+                    $result = $stmt->execute();
+
+                    if ($result) {
+
+                        header("Location: equipamentos.php");
+
+                    } else {
+
+                        throw new Exception("Não foi possível inserir os dados ");
+
+                    }
+
+                } else {
+
+                    header("Location: equipamentos.php");
+
                 }
+
+            } else if (isset($_POST['sub_marca'])) {
+
+                $marca = $_POST['dd_marca'];
+
+                $sql = "DELETE FROM TAB_MARCA WHERE NOME = '$marca'";
+                $result = $conn->query($sql);
+
+                if ($result) {
+
+                    header("Location: equipamentos.php");
+
+                } else {
+
+                    throw new Exception("Não foi possível inserir os dados ");
+
+                } 
+
+            } else if (isset($_POST['add_modelo'])) {
+
+                $modelo = $_POST['txt_modelo'];
+
+                if($modelo != NULL) {
+
+                    $stmt = $conn->prepare("INSERT INTO TAB_MODELO (NOME)
+                    VALUES (?)");
+                    $stmt->bind_param('s', $modelo);
+                    $result = $stmt->execute();
+
+                    if ($result) {
+
+                        header("Location: equipamentos.php");
+
+                    } else {
+
+                        throw new Exception("Não foi possível inserir os dados ");
+
+                    }
+
+                } else {
+
+                    header("Location: equipamentos.php");
+
+                }
+
+
+            } else if (isset($_POST['sub_modelo'])) {
+
+                $modelo = $_POST['dd_modelo'];
+
+                $sql = "DELETE FROM TAB_MODELO WHERE NOME = '$modelo'";
+                $result = $conn->query($sql);
+
+                if ($result) {
+
+                    header("Location: equipamentos.php");
+
+                } else {
+
+                    throw new Exception("Não foi possível inserir os dados ");
+
+                } 
 
             }
 
@@ -77,7 +166,7 @@
 
     }
 
-
+    $conn->close();
 
 ?>
 
