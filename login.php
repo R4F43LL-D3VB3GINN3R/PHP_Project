5220a61715ad11bb519817261bb71b65014f4125
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-29 19:25:05 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../imgs/ipicon.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="../style/login.css">
+    <title>Admin</title>
+</head>
+<body>
+    <div>
+        <form action="login_acesso.php" method="post">
+            <img src="../imgs/ipicon.jpg" alt="">
+            <label for="">Login</label>
+            <input type="text" name="txt_login">
+            <label for="">Pass</label>
+            <input type="password" name="txt_password">
+            <input type="submit" value="Enter" name="login" id="login">
+        </form>
+    </div>
+</body>
+</html>
