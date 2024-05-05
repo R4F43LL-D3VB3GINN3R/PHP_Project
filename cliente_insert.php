98ae106bbbdaac3ebf872878371ddfa45e210eb1
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-05-05 21:17:19 +0100

    Update cliente_insert.php

--- cliente_insert.php
+++ cliente_insert.php
@@ -1,155 +1,258 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8">  
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../imgs/ipicon.jpg" type="image/x-icon">
     <link rel="stylesheet" href="../style/messagebox.css">
-    <title>Clientes</title>
+    <title>Equipamentos</title>
 </head>
 <body>
+    
+<?php 
 
-    <?php
+    try {
 
         include 'conexao.php';
+        include 'permissoes.php';
 
-        try {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-            if ($_SERVER["REQUEST_METHOD"] === "GET") {
+            $nick = $_POST['nick'];
+            $ativo = "Ativo";
+            $inativo = "Inativo";
 
-                $numCli = $_GET['txt_n_cliente'];
-                $nomeCli = $_GET['txt_nomeCliente'];
-                $moradaCli = $_GET['txt_moradaCliente'];
-                $codPostalCli = $_GET['txt_codPostalCliente'];
-                $localCli = $_GET['dd_localidadeCliente'];
-                $telMovelCli = $_GET['txt_telemovelCliente'];
-                $telFoneCli = $_GET['txt_telefoneCliente'];
-                $emailCli = $_GET['txt_emailCliente'];
-                $nifCli = $_GET['txt_nifCliente'];
-                $statusCli = $_GET['dd_statusCliente'];
-                $contratoCli = $_GET['dd_contratoCliente'];
-                $modalidadeCli = $_GET['dd_tipoModalidadeCliente'];
-                $obsCli = $_GET['area_observacoes'];
-                $nick = $_GET['nick'];
+            //Se o formulário for para adicionar tipo...
 
-                //Consulta a linha referente ao número do cliente...
+            if (isset($_POST['add_tipo'])) {
 
-                $sql = "SELECT N_CLIENTE FROM TAB_CLIENTE WHERE N_CLIENTE = '$numCli'";
-                $result = $conn->query($sql);
+                $tipo = $_POST['txt_tipo'];
 
-                if ($result->num_rows > 0) {
+                if ($tipo != NULL) {
 
-                    echo "<div>
-                                <h2>Cliente $numCli já existe em nosso sistema</h2>
-                                <button type='button' onclick='redirect()'>Ok</button>
-                        </div>";
+                    $stmt = $conn->prepare("INSERT INTO TAB_TIPO (NOME, STATUS)
+                    VALUES (?, ?)");
+                    $stmt->bind_param('ss', $tipo, $ativo);
+                    $result = $stmt->execute();
 
-                } else { //Se este cliente não existe em nossa base de dados...
+                    if ($result) {
 
-                    //Convertemos o id da localidade...
-
-                    $sql = "SELECT ID FROM TAB_LOCALIDADE WHERE NOME = '$localCli'";
-                    $result = $conn->query($sql);
-
-                    if ($result->num_rows > 0) {
-
-                        while($row = $result->fetch_assoc()) {
-
-                            $localCli = $row['ID'];
-
-                        }
+                        repasseNick($nick, "equipamentos.php");    
+                        exit;
 
                     } else {
 
-                        throw new Exception("[Erro 401] ao Buscar Informações de Localidade");
+                        throw new Exception("Não foi possível inserir os dados ");
 
                     }
 
-                    if ($contratoCli == "Sim") { //Se o cliente quer um contrato...
+                } else {
 
-                        $tipoContratoCli = $_GET['dd_tipoContratoCliente'];
-                        $tempoTotalCli = $_GET['txt_tempoTotalCliente'];
-                        $tempoExtraCli = $_GET['txt_tempoExtraCliente'];
-                        $modalidadeCli = $_GET['dd_tipoModalidadeCliente'];
-                        $deslocacaoCli = $_GET['dd_deslocacaoCliente'];
-
-                        //Inserimos tanto o contrato do cliente quanto ele mesmo...
-
-                        $stmt = $conn->prepare("INSERT INTO TAB_CONTRATO (ID, TIPO_CONTRATO, TEMPO_TOTAL, TEMPO_EXTRA, DESLOCACAO, MODALIDADE)
-                        VALUES (?, ?, ?, ?, ?, ?)");
-                        $stmt->bind_param('isiiss', $nifCli, $tipoContratoCli, $tempoTotalCli, $tempoExtraCli, $deslocacaoCli, $modalidadeCli);
-                        $resultContrato = $stmt->execute(); 
-
-                        $stmt = $conn->prepare("INSERT INTO TAB_CLIENTE (N_CLIENTE, NIF, NOME, MORADA, CODIGO_POSTAL, ID_LOCALIDADE, CONTACTO_F, CONTACTO_M, CRIADO_DATA, ID_CONTRATO, EMAIL, STATUS, OBSERVACOES)
-                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, ?, ?, ?)");
-                        $stmt->bind_param('iisssississs', $numCli, $nifCli, $nomeCli, $moradaCli, $codPostalCli, $localCli, $telFoneCli, $telMovelCli, $nifCli, $emailCli, $statusCli, $obsCli);
-                        $resultCadastro = $stmt->execute(); 
-
-                        if ($resultCadastro && $resultContrato) {
-
-                            echo "<div>
-                                    <h2>Cliente $numCli Cadastrado</h2>
-                                    <button type='button' onclick='redirect()'>Ok</button>
-                                </div>";
-
-                        } else {
-                        
-                            throw new Exception("[Erro 402] ao Cadastrar Cliente");
-
-                        }
-
-                    } else { //Se o cliente preferir não ter um contrato... 
-
-                        //Inserimos apenas o cliente...
-
-                        $stmt = $conn->prepare("INSERT INTO TAB_CLIENTE (N_CLIENTE, NIF, NOME, MORADA, CODIGO_POSTAL, ID_LOCALIDADE, CONTACTO_F, CONTACTO_M, CRIADO_DATA, EMAIL, STATUS, OBSERVACOES)
-                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, ?, ?)");
-                        $stmt->bind_param('iisssisssss', $numCli, $nifCli, $nomeCli, $moradaCli, $codPostalCli, $localCli, $telFoneCli, $telMovelCli, $emailCli, $statusCli, $obsCli);
-                        $resultCadastro = $stmt->execute(); 
-
-                        if ($resultCadastro) {
-                            
-                            echo "<div>
-                                    <h2>Cliente $numCli Cadastrado</h2>
-                                    <button type='button' onclick='redirect()'>Ok</button>
-                                </div>";
-
-                        } else {
-                            
-                            throw new Exception("[Erro 402] ao Cadastrar Cliente");
-
-                        }
-
-                   }
+                    repasseNick($nick, "equipamentos.php");    
+                    exit;
 
                 }
-                    
-            } else {
 
-                throw new Exception("[Erro 400] na Transmissão de Informações Web ");
+            //Se o formulário for para remover tipo...
+
+            } else if (isset($_POST['sub_tipo'])) {
+
+                $tipo = $_POST['dd_tipo'];
+
+                $stmt = $conn->prepare("UPDATE TAB_TIPO
+                                        SET STATUS = ?
+                                        WHERE NOME = ?");
+                $stmt->bind_param('ss', $inativo, $tipo);
+                $result = $stmt->execute();
+
+                if ($result) {
+
+                    repasseNick($nick, "equipamentos.php");    
+                    exit;
+
+                } else {
+
+                    throw new Exception("Não foi possível inserir os dados ");
+
+                } 
+
+            //Se o formulário for para adicionar marca...
+
+            } else if (isset($_POST['add_marca'])) {
+
+                $marca = $_POST['txt_marca'];
+
+                if($marca != NULL) {
+
+                    $stmt = $conn->prepare("INSERT INTO TAB_MARCA (NOME, STATUS)
+                    VALUES (?, ?)");
+                    $stmt->bind_param('ss', $marca, $ativo);
+                    $result = $stmt->execute();
+
+                    if ($result) {
+
+                        repasseNick($nick, "equipamentos.php");    
+                        exit;
+
+                    } else {
+
+                        throw new Exception("Não foi possível inserir os dados ");
+
+                    }
+
+                } else {
+
+                    repasseNick($nick, "equipamentos.php");    
+                    exit;
+
+                }
+
+            //Se o formulário for para remover marca...
+
+            } else if (isset($_POST['sub_marca'])) {
+
+                $marca = $_POST['dd_marca'];
+
+                $stmt = $conn->prepare("UPDATE TAB_MARCA
+                                        SET STATUS = ?
+                                        WHERE NOME = ?");
+                $stmt->bind_param("ss", $inativo, $marca);
+                $result = $stmt->execute();
+
+                if ($result) {
+
+                    repasseNick($nick, "equipamentos.php");    
+                    exit;
+
+                } else {
+
+                    throw new Exception("Não foi possível inserir os dados ");
+
+                } 
+
+            //Se o formulário for para adicionar modelo...
+
+            } else if (isset($_POST['add_modelo'])) {
+
+                $modelo = $_POST['txt_modelo'];
+
+                if($modelo != NULL) {
+
+                    $stmt = $conn->prepare("INSERT INTO TAB_MODELO (NOME, STATUS)
+                    VALUES (?, ?)");
+                    $stmt->bind_param('ss', $modelo, $ativo);
+                    $result = $stmt->execute();
+
+                    if ($result) {
+
+                        repasseNick($nick, "equipamentos.php");    
+                        exit;
+
+                    } else {
+
+                        throw new Exception("Não foi possível inserir os dados ");
+
+                    }
+
+                } else {
+
+                    repasseNick($nick, "equipamentos.php");    
+                    exit;
+
+                }
+
+            //Se o formulário for para remover modelo...
+
+
+            } else if (isset($_POST['sub_modelo'])) {
+
+                $modelo = $_POST['dd_modelo'];
+
+                $stmt = $conn->prepare("UPDATE TAB_MODELO
+                                        SET STATUS = ?
+                                        WHERE NOME = ?");
+                $stmt->bind_param("ss", $inativo, $modelo);
+                $result = $stmt->execute();
+
+                if ($result) {
+
+                    repasseNick($nick, "equipamentos.php");    
+                    exit;
+
+                } else {
+
+                    throw new Exception("Não foi possível inserir os dados ");
+
+                }
+                
+            //Se o formulário for para adicionar estado...
+
+            } else if (isset($_POST['add_estado'])) {
+
+                $estado = $_POST['txt_estado'];
+
+                if($estado != NULL) {
+
+                    $stmt = $conn->prepare("INSERT INTO TAB_ESTADO (NOME)
+                    VALUES (?)");
+                    $stmt->bind_param('s', $estado);
+                    $result = $stmt->execute();
+
+                    if ($result) {
+
+                        repasseNick($nick, "equipamentos.php");    
+                        exit;
+
+                    } else {
+
+                        throw new Exception("Não foi possível inserir os dados ");
+
+                    }
+
+                } else {
+
+                    repasseNick($nick, "equipamentos.php");    
+                    exit;
+
+                }
+
+            //Se o formulário for para remover estado...
+
+            } else if (isset($_POST['sub_estado'])) {
+
+                $estado = $_POST['dd_estado'];
+
+                $sql = "DELETE FROM TAB_ESTADO WHERE NOME = '$estado'";
+                $result = $conn->query($sql);
+
+                if ($result) {
+
+                    repasseNick($nick, "equipamentos.php");    
+                    exit;
+
+                } else {
+
+                    throw new Exception("Não foi possível inserir os dados ");
+
+                } 
 
             }
 
-        } catch (Exception $e) {
+        } else {
 
-            throw new Exception("[Erro 400] na Transmissão de Informações Web ");
+            throw new Exception("[Erro 400] na Transmissão do Método Post ");
 
         }
 
         $conn->close();
-       
-    ?>
 
-    <script>
+    } catch (Exception $e) {
 
-    var nick = '<?php echo $nick; ?>';
-
-    function redirect() {
-
-        window.location.href = 'cliente.php?nick=' + nick;
+        throw new Exception("[Erro 400] na Transmissão de Informações Web ");
 
     }
-        
-    </script>
+
+?>
+
 </body>
 </html>
