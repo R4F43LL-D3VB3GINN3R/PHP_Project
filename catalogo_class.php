1c3ab75ccc878af88c02e5ff7d086f42431d3e3d
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-05-16 16:59:32 +0100

    Update catalogo_class.php

--- catalogo_class.php
+++ catalogo_class.php
@@ -2,6 +2,68 @@
 
 class catalogo {
 
+    public function recebeNick_Catalogo($nickname, $id_material) {
+
+        $data = array();
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            if(isset($_POST['nick'])) {
+
+                $data['nick'] = $_POST['nick'];
+
+            }
+
+            if(isset($_POST['id_cat'])) {
+
+                $data['id_cat'] = $_POST['id_cat'];
+
+            }
+
+            if(isset($_POST['formnick'])) {
+
+                $data['nick'] = $_POST['formnick'];
+
+            }
+
+            if(isset($_POST['formid'])) {
+
+                $data['id_cat'] = $_POST['formid'];
+
+            }
+
+        } elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+            if(isset($_GET['nick'])) {
+
+                $data['nick'] = $_GET['nick'];
+
+            }
+
+            if(isset($_GET['id_cat'])) {
+
+                $data['id_cat'] = $_GET['id_cat'];
+
+            }
+
+            if(isset($_GET['formnick'])) {
+
+                $data['nick'] = $_GET['formnick'];
+
+            }
+
+            if(isset($_GET['formid'])) {
+
+                $data['id_cat'] = $_GET['formid'];
+                
+            }
+
+        }
+
+        return $data;
+
+    }
+
     public function displayCatalogo($nickname) {
 
         include 'conexao.php';
