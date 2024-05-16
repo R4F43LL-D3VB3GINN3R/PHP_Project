9338874dc1bf34abad791c3ae62c5fb15f1239f4
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-05-16 14:52:05 +0100

    Create catalogo_class.php

---
+++ catalogo_class.php
@@ -0,0 +1,43 @@
+<?php 
+
+class catalogo {
+
+    public function displayCatalogo($nickname) {
+
+        include 'conexao.php';
+
+        $sql = "SELECT * FROM TAB_CATALOGO";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+
+            while ($row = $result->fetch_assoc()) {
+
+
+                echo "<tr>";
+                echo "<td>" . $row['ID'] . "</td>";
+                echo "<td>" . $row['NOME'] . "</td>";
+                echo "<td>" . $row['DESCRICAO'] . "</td>";
+                echo "<td>" . $row['PRECO'] . "</td>";
+                echo "<td><form action='catalogo_gerenciamento.php' method='get'><input type='hidden' name='id_cat' value='" . $row['ID'] . "'><input type='submit' value='Editar' id='managecat'><input type='hidden' name='nick' value='" . $nickname . "'></form></td>";
+                echo "</tr>";
+                
+
+            } 
+
+            echo "<tr>";
+                echo "<td>" . "--" . "</td>";
+                echo "<td>" . "--" . "</td>";
+                echo "<td>" . "--" . "</td>";
+                echo "<td>" . "--" . "</td>";
+            echo "<td><form action='catalogo_gerenciamento.php' method='get'><input type='submit' value='+' id='managecat'><input type='hidden' name='nick' value='" . $nickname . "'></form></td>";
+            
+            $conn->close();
+
+        }
+
+    }
+
+}
+
+?>
