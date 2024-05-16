2191cd8c61a96378afcf64e7c43379e4146814ca
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-05-17 00:41:18 +0100

    Update equipamento_class.php

--- equipamento_class.php
+++ equipamento_class.php
@@ -3,8 +3,6 @@
 class equipamento {
 
     function display_equipamento($tabela) {
-        
-        echo '<select name="dd_tipo" id="tipo">';
 
         include 'conexao.php';
 
@@ -20,10 +18,79 @@ class equipamento {
             }
 
         }
-        
-        echo '</select>';
 
         $conn->close();
+
+    }
+
+    function add_equipamento($form, $request, $table, $nick) {
+
+        include 'conexao.php';
+
+        if (isset($_POST[$form])) {
+
+            $request = $_POST[$request];
+            $ativo = "Ativo";
+
+            if ($request != NULL) {
+
+                $stmt = $conn->prepare("INSERT INTO $table (NOME, STATUS) VALUES (?, ?)");
+                $stmt->bind_param('ss', $request, $ativo);
+                $result = $stmt->execute();
+
+                if ($result) {
+
+                    repasseNick($nick, "equipamentos.php");    
+                    exit;
+
+                } else {
+
+                    throw new Exception("Não foi possível inserir os dados");
+
+                }
+
+            } else {
+
+                repasseNick($nick, "equipamentos.php");    
+                exit;
+
+            }
+
+            $conn->close();
+
+        }
+
+    }
+
+    function remove_equipamento($form, $request, $table, $nick) {
+
+        include 'conexao.php';
+
+        if (isset($_POST[$form])) {
+
+            $request = $_POST[$request];
+            $inativo = 'Inativo';
+
+            $stmt = $conn->prepare("UPDATE $table
+                                    SET STATUS = ?
+                                    WHERE NOME = ?");
+            $stmt->bind_param('ss', $inativo, $request);
+            $result = $stmt->execute();
+
+            if ($result) {
+
+                repasseNick($nick, "equipamentos.php");    
+                exit;
+
+            } else {
+
+                throw new Exception("Não foi possível inserir os dados ");
+
+            } 
+
+            $conn->close();
+
+        }
 
     }
 
